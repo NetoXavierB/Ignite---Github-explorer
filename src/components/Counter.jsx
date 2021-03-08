7b9a92458aad37b52f7ed87b175d723d06ff1652
Neto Xavier <j.x.b.n@hotmail.com>
2021-03-08 14:21:44 -0300

    Instalção plugin react refresh webpack

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -14,7 +14,7 @@ export function Counter() {
                 type="button"
                 onClick={increment}
             >
-                Increment
+                Increment +1
             </button>
         </div>
     )
